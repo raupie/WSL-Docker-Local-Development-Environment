7db38e60a27104f0259f8a8c6f5d335047b89a20
Travis Raup <raupie@users.noreply.github.com>
2017-11-29 19:52:02 -0500

    Create ezlaunchpad-install.bash

---
+++ ezlaunchpad-install.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ezlaunchpad installation + dependencies
+
+# Install PHP
+sudo apt-get install -y php php-cli php-curl
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+# Download + Install ezlaunchpad
+curl -LSs https://ezsystems.github.io/launchpad/install_curl.bash | bash
+mv .ezlaunchpad /usr/local/bin/.ezlaunchpad
+ln -sf /usr/local/bin/.ezlaunchpad/ez.phar ez
+mv ez /usr/local/bin
