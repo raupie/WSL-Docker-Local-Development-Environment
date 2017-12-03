0ed96b03d2fbd983b5d7bfcb0283284996caf6f4
Travis Raup <raupie@users.noreply.github.com>
2017-12-03 13:03:18 -0500

    Update docker-sync-installation.bash

--- docker-sync-installation.bash
+++ docker-sync-installation.bash
@@ -8,7 +8,7 @@ sudo apt-get install ruby ruby-dev
 sudo gem install docker-sync
 
 # Install unison
-sudo apt-get install ocaml make emacs
+sudo apt-get install ocaml make emacs -y
 wget https://github.com/bcpierce00/unison/archive/2.48.4.tar.gz
 tar xvf 2.48.4.tar.gz
 cd unison-2.48.4
